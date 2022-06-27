a414ab248159e606834d639160752eeb9778c999
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-06-27 21:59:35 +0700

    done

--- src/main/java/com/codegym/test/model/entity/Product.java
+++ src/main/java/com/codegym/test/model/entity/Product.java
@@ -26,17 +26,21 @@ public class Product {
     private String image;
 
     @ManyToOne
+    private User user;
+
+    @ManyToOne
     private Category category;
 
     @ManyToOne
     private Branch branch;
 
-    public Product(String name, double price, String description, int quantity, String image, Category category, Branch branch) {
+    public Product(String name, double price, String description, int quantity, String image, User user, Category category, Branch branch) {
         this.name = name;
         this.price = price;
         this.description = description;
         this.quantity = quantity;
         this.image = image;
+        this.user = user;
         this.category = category;
         this.branch = branch;
     }
